32bc6c45cc9643ae8e70786b27d021c60744c32e
Waqas Umar <55554486+WAQASUMAR33@users.noreply.github.com>
2024-07-08 21:00:59 +0500

    first commit

--- src/app/admin/pages/layout.js
+++ src/app/admin/pages/layout.js
@@ -1,19 +1,18 @@
-import Sidebar from '../components/Header';
-import Header from '../components/Sidebar';
+import Sidebar from '../components/Sidebar';
+import Header from '../components/Header';
+
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-row ">
+    <div className="flex flex-row">
       <div>
-      <Sidebar/>
+        <Sidebar />
       </div>
-      <div className="flex flex-col w-full className={inter.className}">
-      <Header/>
-      {children}
+      <div className={`flex flex-col `}>
+        <Header />
+        {children}
       </div>
-
     </div>
-
   );
 };
 
